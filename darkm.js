2b9f40e76a1b3e42d0632de3bbcfc83c6a7008f5
Renia Xyth <118560462+axlna@users.noreply.github.com>
2023-05-22 13:35:31 +0300

    dark mode finish
    user perfrence remembered now

--- darkm.js
+++ darkm.js
@@ -2,8 +2,19 @@ function toggleDarkMode() {
     var body = document.getElementsByTagName("body")[0];
     var currentClass = body.className;
     if (currentClass === "dark-mode") {
-        body.className = "";
+      body.className = "";
+      localStorage.setItem("darkMode", "");
     } else {
-        body.className = "dark-mode";
+      body.className = "dark-mode";
+      localStorage.setItem("darkMode", "enabled");
     }
-}
+  }
+  
+  window.addEventListener("DOMContentLoaded", function () {
+    var darkMode = localStorage.getItem("darkMode");
+    var body = document.getElementsByTagName("body")[0];
+    if (darkMode === "enabled") {
+      body.className = "dark-mode";
+    }
+  });
+  
