a0e4111a9fc0ceaf4c2dd6c123df7e8942cf3f56
Renia Xyth <reniaxythali@gmail.com>
2023-05-28 12:44:43 +0300

    .

--- darkm.js
+++ darkm.js
@@ -1,22 +1,20 @@
 function toggleDarkMode() {
-  var body = document.getElementsByTagName("body")[0];
-  var currentClass = body.className;
-  if (currentClass === "dark-mode") {
-    body.className = "";
-    localStorage.setItem("darkMode", "");
-  } else {
-    body.className = "dark-mode";
-    localStorage.setItem("darkMode", "enabled");
+    var body = document.getElementsByTagName("body")[0];
+    var currentClass = body.className;
+    if (currentClass === "dark-mode") {
+      body.className = "";
+      localStorage.setItem("darkMode", "");
+    } else {
+      body.className = "dark-mode";
+      localStorage.setItem("darkMode", "enabled");
+    }
   }
-}
-
-window.addEventListener("DOMContentLoaded", function () {
-  var darkMode = localStorage.getItem("darkMode");
-  var body = document.getElementsByTagName("body")[0];
-  if (darkMode === "enabled") {
-    body.className = "dark-mode";
-  } else {
-    body.className = "dark-mode"; // Enable dark mode by default
-    localStorage.setItem("darkMode", "enabled");
-  }
-});
+  
+  window.addEventListener("DOMContentLoaded", function () {
+    var darkMode = localStorage.getItem("darkMode");
+    var body = document.getElementsByTagName("body")[0];
+    if (darkMode === "enabled") {
+      body.className = "dark-mode";
+    }
+  });
+  
