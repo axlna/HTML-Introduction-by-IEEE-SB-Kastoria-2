32051f7f2c71e347632cf3ff2dce6671eaab366f
Renia Xyth <reniaxythali@gmail.com>
2023-06-03 19:23:15 +0300

    7:20

--- contact.js
+++ contact.js
@@ -1,25 +1,16 @@
-form.addEventListener('submit', function(event) {
-    event.preventDefault(); 
-  
-    console.log('Form submitted!'); 
-
 document.addEventListener('DOMContentLoaded', function() {
     const form = document.querySelector('form[name="contact"]');
     const nameInput = document.getElementById('name');
     const emailInput = document.getElementById('email');
     const messageInput = document.getElementById('message');
-
+  
     form.addEventListener('submit', function(event) {
       event.preventDefault(); 
-
-      
-
-      
-      nameInput.value = '';
-      emailInput.value = '';
-      messageInput.value = '';
-
-      
+  
+      console.log('Form submitted!'); 
+  
+      form.reset();
       alert('Message sent!');
     });
-  })});
+  });
+  
